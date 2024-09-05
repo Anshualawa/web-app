155124b04cd29ed43aeea6f69b438431a90935e0
Anshualawa <alawapappu@gmail.com>
2024-09-05 17:16:23 +0530

    modify Home page component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,303 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div>
-      <h2 className="font-mono text-2xl font-bold">Home Page</h2>
-      <p>Welcome to the Home Page!</p>
-    </div>
+    <>
+      <div className="px-2 py-2 md:px-6 md:py-10">
+        <h1 className="text-2xl font-bold capitalize text-black lg:text-3xl">
+          DevUI Makes Your Work Easier
+        </h1>
+        <p className="my-2 text-gray-600">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum quam
+          voluptatibus
+        </p>
+        <hr />
+        <div className="mt-8 md:p-5 grid grid-cols-1 gap-8 md:grid-cols-2 xl:mt-12 xl:grid-cols-3 xl:gap-16" style={{ backgroundImage: "url('./building.jpeg')", backgroundSize: 'cover', backgroundPosition: 'center' }}>
+          <div className="space-y-3 md:p-5 rounded-xl shadow bg-indigo-200">
+            <span className="inline-block rounded-full bg-gray-100 p-3 text-black">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-copy"
+              >
+                <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
+                <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
+              </svg>
+            </span>
+            <h1 className="text-xl font-semibold capitalize text-black">
+              Copy &amp; paste components
+            </h1>
+            <p className="text-sm text-gray-500">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ab
+              nulla quod dignissimos vel non corrupti doloribus voluptatum eveniet
+            </p>
+            <a
+              href="#"
+              className="-mx-1 inline-flex transform items-center text-sm font-semibold capitalize text-black transition-colors duration-300 hover:underline"
+            >
+              <span className="mx-1">read more</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-arrow-right"
+              >
+                <line x1="5" y1="12" x2="19" y2="12"></line>
+                <polyline points="12 5 19 12 12 19"></polyline>
+              </svg>
+            </a>
+          </div>
+          <div className="space-y-3 md:p-5 rounded-xl shadow bg-indigo-200">
+            <span className="inline-block rounded-full bg-gray-100 p-3 text-black">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-copy"
+              >
+                <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
+                <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
+              </svg>
+            </span>
+            <h1 className="text-xl font-semibold capitalize text-black">
+              Copy &amp; paste components
+            </h1>
+            <p className="text-sm text-gray-500">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ab
+              nulla quod dignissimos vel non corrupti doloribus voluptatum eveniet
+            </p>
+            <a
+              href="#"
+              className="-mx-1 inline-flex transform items-center text-sm font-semibold capitalize text-black transition-colors duration-300 hover:underline"
+            >
+              <span className="mx-1">read more</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-arrow-right"
+              >
+                <line x1="5" y1="12" x2="19" y2="12"></line>
+                <polyline points="12 5 19 12 12 19"></polyline>
+              </svg>
+            </a>
+          </div>
+          <div className="space-y-3 md:p-5 rounded-xl shadow bg-indigo-200">
+            <span className="inline-block rounded-full bg-gray-100 p-3 text-black">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-copy"
+              >
+                <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
+                <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
+              </svg>
+            </span>
+            <h1 className="text-xl font-semibold capitalize text-black">
+              Copy &amp; paste components
+            </h1>
+            <p className="text-sm text-gray-500">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ab
+              nulla quod dignissimos vel non corrupti doloribus voluptatum eveniet
+            </p>
+            <a
+              href="#"
+              className="-mx-1 inline-flex transform items-center text-sm font-semibold capitalize text-black transition-colors duration-300 hover:underline"
+            >
+              <span className="mx-1">read more</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-arrow-right"
+              >
+                <line x1="5" y1="12" x2="19" y2="12"></line>
+                <polyline points="12 5 19 12 12 19"></polyline>
+              </svg>
+            </a>
+          </div>
+          <div className="space-y-3 md:p-5 rounded-xl shadow bg-indigo-200" >
+            <span className="inline-block rounded-full bg-gray-100 p-3 text-black">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-copy"
+              >
+                <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
+                <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
+              </svg>
+            </span>
+            <h1 className="text-xl font-semibold capitalize text-black">
+              Copy &amp; paste components
+            </h1>
+            <p className="text-sm text-gray-500">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ab
+              nulla quod dignissimos vel non corrupti doloribus voluptatum eveniet
+            </p>
+            <a
+              href="#"
+              className="-mx-1 inline-flex transform items-center text-sm font-semibold capitalize text-black transition-colors duration-300 hover:underline"
+            >
+              <span className="mx-1">read more</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-arrow-right"
+              >
+                <line x1="5" y1="12" x2="19" y2="12"></line>
+                <polyline points="12 5 19 12 12 19"></polyline>
+              </svg>
+            </a>
+          </div>
+          <div className="space-y-3 md:p-5 rounded-xl shadow bg-indigo-200">
+            <span className="inline-block rounded-full bg-gray-100 p-3 text-black">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-copy"
+              >
+                <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
+                <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
+              </svg>
+            </span>
+            <h1 className="text-xl font-semibold capitalize text-black">
+              Copy &amp; paste components
+            </h1>
+            <p className="text-sm text-gray-500">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ab
+              nulla quod dignissimos vel non corrupti doloribus voluptatum eveniet
+            </p>
+            <a
+              href="#"
+              className="-mx-1 inline-flex transform items-center text-sm font-semibold capitalize text-black transition-colors duration-300 hover:underline"
+            >
+              <span className="mx-1">read more</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-arrow-right"
+              >
+                <line x1="5" y1="12" x2="19" y2="12"></line>
+                <polyline points="12 5 19 12 12 19"></polyline>
+              </svg>
+            </a>
+          </div>
+          <div className="space-y-3 md:p-5 rounded-xl shadow bg-indigo-200">
+            <span className="inline-block rounded-full bg-gray-100 p-3 text-black">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-copy"
+              >
+                <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
+                <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
+              </svg>
+            </span>
+            <h1 className="text-xl font-semibold capitalize text-black">
+              Copy &amp; paste components
+            </h1>
+            <p className="text-sm text-gray-500">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ab
+              nulla quod dignissimos vel non corrupti doloribus voluptatum eveniet
+            </p>
+            <a
+              href="#"
+              className="-mx-1 inline-flex transform items-center text-sm font-semibold capitalize text-black transition-colors duration-300 hover:underline"
+            >
+              <span className="mx-1">read more</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-arrow-right"
+              >
+                <line x1="5" y1="12" x2="19" y2="12"></line>
+                <polyline points="12 5 19 12 12 19"></polyline>
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+
+    </>
   );
 };
 
