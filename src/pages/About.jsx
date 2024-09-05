68ce09e2609a7c0019f290e20473706a29dc038b
Anshualawa <alawapappu@gmail.com>
2024-09-05 16:15:01 +0530

    update contact component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,6 +5,7 @@ const About = () => {
     <div>
       <h2 className="text-2xl font-bold">About Page</h2>
       <p>Learn more about us on this page.</p>
+      <p className="text-gray-500 text-xl">contact:- alawa@gmail.com</p>
     </div>
   );
 };
